8bd6ef4dcd196a63558ffbed89c6c5b42eff2cd9
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-07-29 16:37:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet() {}
+// - If the time is earlier than 12pm, return "Good Morning".
+// - If the time is between 12pm and 5pm, return "Good Afternoon".
+// - If the time is later than 5pm, return "Good Evening".
+function greet(timeIn24) {
+  // HH:MM format
+  let hours = parseInt(timeIn24);
+  if (hours < 12) {
+    return "Good Morning";
+  } else if (hours >= 12 && hours <= 17) {
+    return "Good Afternoon";
+  } else if (hours > 17) {
+    return "Good Evening";
+  }
+  // return(hours);
+}
 /* Write your implementation of displayMessage() */
-function displayMessage() {}
+function displayMessage(string) {
+  const greeting = document.querySelector("#greetings");
+  greeting.innerText = string;
+}
+
